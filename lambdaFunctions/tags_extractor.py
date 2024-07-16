daa253c0bb4bb472066b47bad03aa33ddbd6cad4
Radhi Hudijan <radhif2009@gmail.com>
2024-07-16 20:02:52 +0200

    Add code for extracting and storing EC2 instance tags

--- lambdaFunctions/tags_extractor.py
+++ lambdaFunctions/tags_extractor.py
@@ -1,15 +1,28 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # Extract data from the event
-    # TODO: Add your code here
-    
-    # Process the data
-    # TODO: Add your code here
-    
-    # Return a response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-    return response
+    dynamodb = boto3.resource('dynamodb')
+    table_name = 'ec2_instance_tags'
+
+    instance_id = event['detail']['requestParameters']['resourcesSet']['items'][0]['resourceId']
+    tags = event['detail']['requestParameters']['tagSet']['items']
+
+    try:
+        table = dynamodb.Table(table_name)
+        for tag in tags:
+            if tag['key'] in ['auto:stop', 'auto:start']:
+                table.put_item(
+                    Item={
+                        'instance_id': instance_id,
+                        'tag_key': tag['key'],
+                        'tag_value': tag['value']
+                    }
+                )
+
+    except Exception as e:
+        print(f'Error: {e}')
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Internal Server Error')
+        }
