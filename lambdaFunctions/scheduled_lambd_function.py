d45ec250e9c610c6795a5bde868cc5c71c93b183
Radhi Hudijan <radhif2009@gmail.com>
2024-07-23 17:00:31 +0200

    Refactor current_time variable in lambda function

--- lambdaFunctions/scheduled_lambd_function.py
+++ lambdaFunctions/scheduled_lambd_function.py
@@ -1,6 +1,7 @@
 import datetime
 import json
-import boto3
+import boto3  
+import pytz
 
 def lambda_handler(event, context):
     #get the service resource
@@ -13,9 +14,10 @@ def lambda_handler(event, context):
     response = table.scan()
     items = response['Items']
 
-    current_time = datetime.datetime.now()
-    current_time_str = current_time.strftime('%H:%M')
-    print(f"Current time: {current_time_str}")
+    #get the current time
+    # current_time = datetime.datetime.now()
+    # current_time_str = current_time.strftime('%H:%M')
+    # print(f"Current time: {current_time_str}")
     
     #check if the current time matches the time in the tags
     for item in items:
@@ -23,17 +25,66 @@ def lambda_handler(event, context):
         tag_key=item['tag_key']
         tag_value=item['tag_value']
 
-        #convert the tag value to datetime object
-        tag_time = datetime.datetime.strptime(tag_value, '%H:%M')
-        tag_time = tag_time.replace(year=current_time.year, month=current_time.month, day=current_time.day)
+        try:
+            # Get the instance region and timezone
+            instance = ec2.describe_instances(InstanceIds=[instance_id])
+            region = instance['Reservations'][0]['Instances'][0]['Placement']['AvailabilityZone'][:-1]
+            timezone = pytz.timezone(get_timezone(region))
+            
+            # Get the current time in the instance's region
+            current_time = datetime.datetime.now(timezone)
+            current_time_str = current_time.strftime("%H:%M")
+            print(f"Current time in region {region} =", current_time_str)
 
-        #check if the current time matches the tag time 
 
-        if item['tag_key'] == 'auto:stop' and item['tag_value'] == current_time:
-            instance_id = item['instance_id']
-            ec2.stop_instances(InstanceIds=[instance_id])
-            print(f"Instance {instance_id} stopped at {current_time}")
-        elif item['tag_key'] == 'auto:start' and item['tag_value'] == current_time:
-            instance_id = item['instance_id']
-            ec2.start_instances(InstanceIds=[instance_id])
-            print(f"Instance {instance_id} started at {current_time}")
+            #convert the tag value to datetime object
+            tag_time = datetime.datetime.strptime(tag_value, '%H:%M')
+            tag_time = tag_time.replace(year=current_time.year, month=current_time.month, day=current_time.day)
+            print(f"{tag_key} time of instance {instance_id}: {tag_time}")
+
+            #check if the tag time has passed
+            if current_time >= tag_time:
+                
+                #get the instance state
+                state = instance['Reservations'][0]['Instances'][0]['State']['Name']
+                print(f"Current Instance state: {state}")
+
+                #stop the instance if it is running
+                if tag_key == "auto:stop":
+                    if state == 'running':
+                        print(f"Stopping instance: {instance_id} at {current_time_str}")
+                        ec2.stop_instances(InstanceIds=[instance_id])
+                    else:
+                        print(f"Instance {instance_id} is already stopped at {current_time_str}") 
+
+                #start the instance if it is stopped
+                elif tag_key == "auto:start":
+                    if state == 'stopped':
+                        print(f"Starting instance: {instance_id} at {current_time_str}")
+                        ec2.start_instances(InstanceIds=[instance_id])
+                    else:
+                        print(f"Instance {instance_id} is already running at {current_time_str}")
+
+        except Exception as e:
+            print(f"Error processing instance {instance_id}: {e}")
+            return {
+                      'statusCode': 500,
+                      'body': json.dumps('Internal Server Error')
+                      }
+
+
+def get_timezone(region):
+    # A mapping of AWS regions to their respective timezones
+    region_timezones = {
+        'us-east-1': 'US/Eastern',
+        'us-east-2': 'US/Eastern',
+        'us-west-1': 'US/Pacific',
+        'us-west-2': 'US/Pacific',
+        'eu-west-1': 'Europe/Dublin',
+        'eu-central-1': 'Europe/Berlin',
+        'eu-west-2': 'Europe/London',
+        'eu-west-3': 'Europe/Paris',
+        'eu-north-1': 'Europe/Helsinki',
+    }
+    return region_timezones.get(region, 'UTC')  # Default to UTC if region is not found
+       
