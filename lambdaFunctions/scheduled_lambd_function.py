91c1e288bc52fb658c4198dd153fc43a78ffffaa
Radhi Hudijan <radhi.hudijan@accenture.com>
2024-09-12 13:34:32 +0200

    chore: Refactor lambda function to improve code readability and add debug statements

--- lambdaFunctions/scheduled_lambd_function.py
+++ lambdaFunctions/scheduled_lambd_function.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 import boto3  
-import pytz
+
 
 def lambda_handler(event, context):
     print("Lambda function started")  # Added debug statement
@@ -12,82 +12,75 @@ def lambda_handler(event, context):
 
     table_name = 'ec2_instance_tags'
     table = dynamodb.Table(table_name)
+    
+    try:
+        #scan the table
+        response = table.scan(
+            FilterExpression=Attr('tag_key').ne('auto:shutdown')
+        )
+        items = response['Items']
+       
 
-
-    response = table.scan()
-    items = response['Items']
-    print(f"Scanned items: {items}")  # Added debug statement
-
+    except Exception as e:
+        print(f'Error: {e}')
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error scanning the table')
+        }
 
     #check if the current time matches the time in the tags
     for item in items:
         instance_id=item['instance_id']
         tag_key=item['tag_key']
         tag_value=item['tag_value']
+        auto_shutdown=item['auto_shutdown']
         
-
-        try:
-            #Get the instance region and timezone
-            instance = ec2.describe_instances(InstanceIds=[instance_id])
-            region = instance['Reservations'][0]['Instances'][0]['Placement']['AvailabilityZone'][:-1]
-            
-            timezone = pytz.timezone(get_timezone(region))
-            print(f"timezon is :{timezone}")
-            
-            # Get the current time in the instance's region
-            current_time = datetime.datetime.now(timezone)
-            current_time_str = current_time.strftime("%H:%M")
-            print(f"Current time in region {region} =", current_time_str)
-
-
-            # convert the tag value to datetime object
-            tag_time = datetime.datetime.strptime(tag_value, '%H:%M')
-            
-            # convert the tag time to timezone-aware datetime object
-            tag_time = timezone.localize(tag_time.replace(year=current_time.year, month=current_time.month, day=current_time.day))
-            print(f"{tag_key} time of instance {instance_id}: {tag_time}")
-
-            #check if the tag time has passed
-            if current_time >= tag_time:
-                
-                #get the instance state
+        #check if the instance is set to auto shutdown
+        if auto_shutdown:
+            print(f"Instance {instance_id} is set to auto shutdown")
+        
+            try:
+                #Get the instance region and timezone
+                instance = ec2.describe_instances(InstanceIds=[instance_id])
                 state = instance['Reservations'][0]['Instances'][0]['State']['Name']
                 print(f"Current Instance state: {state}")
+    
+                # Get the current time in the instance's region
+                current_time = datetime.datetime.utcnow()
+                current_time_str = current_time.strftime("%H:%M")
+                print(f"Current time in UTC =", current_time_str)
 
-                #stop the instance if it is running
-                if tag_key == "auto:stop":
-                    if state == 'running':
-                        print(f"Stopping instance: {instance_id} at {current_time_str}")
-                        ec2.stop_instances(InstanceIds=[instance_id])
-                    else:
-                        print(f"Instance {instance_id} is already stopped at {current_time_str}") 
 
-                #start the instance if it is stopped
-                elif tag_key == "auto:start":
-                    if state == 'stopped':
-                        print(f"Starting instance: {instance_id} at {current_time_str}")
-                        ec2.start_instances(InstanceIds=[instance_id])
-                    else:
-                        print(f"Instance {instance_id} is already running at {current_time_str}")
+                # convert the tag value to datetime object in UTC
+                tag_time = datetime.datetime.strptime(tag_value, '%H:%M')
+                tag_time = tag_time.replace(year=current_time.year, month=current_time.month, day=current_time.day)
+                
 
-        except Exception as e:
-            print(f"Error processing instance {instance_id}: {e}")
-            return {
-                'statusCode': 500,
-                'body': json.dumps('Internal Server Error')
-            }
+                #check if the tag time has passed
+                if current_time >= tag_time:
+                    
+                    #stop the instance if it is running
+                    if tag_key == "auto:stop":
+                        if state == 'running':
+                            print(f"Stopping instance: {instance_id} at {current_time_str}")
+                            ec2.stop_instances(InstanceIds=[instance_id])
+                        else:
+                            print(f"Instance {instance_id} is already stopped at {current_time_str}") 
 
-def get_timezone(region):
-    # A mapping of AWS regions to their respective timezones
-    region_timezones = {
-        'us-east-1': 'US/Eastern',
-        'us-east-2': 'US/Eastern',
-        'us-west-1': 'US/Pacific',
-        'us-west-2': 'US/Pacific',
-        'eu-west-1': 'Europe/Dublin',
-        'eu-central-1': 'Europe/Berlin',
-        'eu-west-2': 'Europe/London',
-        'eu-west-3': 'Europe/Paris',
-        'eu-north-1': 'Europe/Helsinki',
-    }
-    return region_timezones.get(region, 'UTC')
+                    #start the instance if it is stopped
+                    elif tag_key == "auto:start":
+                        if state == 'stopped':
+                            print(f"Starting instance: {instance_id} at {current_time_str}")
+                            ec2.start_instances(InstanceIds=[instance_id])
+                        else:
+                            print(f"Instance {instance_id} is already running at {current_time_str}")
+
+            except Exception as e:
+                print(f"Error processing instance {instance_id}: {e}")
+                return {
+                    'statusCode': 500,
+                    'body': json.dumps('Internal Server Error')
+                }
+
+        else:
+            print(f"Instance {instance_id} is not set to auto shutdown")
--- lambdaFunctions/tags_extractor.py
+++ lambdaFunctions/tags_extractor.py
@@ -3,43 +3,69 @@ import boto3
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
+    ec2 = boto3.client('ec2')
     table_name = 'ec2_instance_tags'
 
     instance_id = event['detail']['requestParameters']['resourcesSet']['items'][0]['resourceId']
     tags = event['detail']['requestParameters']['tagSet']['items']
+    print(f"Tags from event: {tags}")
 
     try:
         table = dynamodb.Table(table_name)
-        for tag in tags:
-            if tag['key'] in ['auto:stop', 'auto:start']:
-                # check if the tag is already present in the table
-                response = table.get_item(
-                    Key={
-                        'instance_id': instance_id,
-                        'tag_key': tag['key']
-                    }
-                )
-                if 'Item' in response:
-                    # update the tag value
-                    table.update_item(
+    
+        #get all the instance tags
+        instance_tags = ec2.describe_tags(
+            Filters=[
+                {
+                    'Name': 'resource-id',
+                    'Values': [instance_id]
+                }
+            ]
+        )
+        print(f"Instance tags: {instance_tags}")
+        auto_shutdown = False
+
+        # Check if 'auto:shutdown' exists and is set to true
+        for tag in instance_tags['Tags']:
+            if tag['Key'] == 'auto:shutdown' and tag['Value'].lower() == 'true':
+                auto_shutdown = True
+
+        
+            for tag in tags:
+                if tag['key'] in ['auto:stop', 'auto:start']:
+                    # check if the tag is already present in the table
+                    response = table.get_item(
                         Key={
                             'instance_id': instance_id,
                             'tag_key': tag['key']
-                        },
-                        UpdateExpression='SET tag_value = :val',
-                        ExpressionAttributeValues={
-                            ':val': tag['value']
-                        }
-                    )
-                else:
-                    # add the tag to the table  
-                    table.put_item(
-                        Item={
-                            'instance_id': instance_id,
-                            'tag_key': tag['key'],
-                            'tag_value': tag['value']
-                        }
-                    )
+                            }
+                        )
+                    if 'Item' in response:
+                        # update the tag value
+                        table.update_item(
+                            Key={
+                                'instance_id': instance_id,
+                                'tag_key': tag['key'],
+                            },
+                            UpdateExpression='SET tag_value = :val, auto_shutdown = :auto_shutdown',
+                            ExpressionAttributeValues={
+                                ':val': tag['value'],
+                                ':auto_shutdown': auto_shutdown
+                            }
+                        )
+                    else:
+                        # add the tag to the table  
+                        table.put_item(
+                            Item={
+                                'instance_id': instance_id,
+                                'tag_key': tag['key'],
+                                'tag_value': tag['value'],
+                                'auto_shutdown': auto_shutdown
+                            }
+                        )
+      
+
+                
     except Exception as e:
         print(f'Error: {e}')
         return {
